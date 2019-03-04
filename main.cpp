0e80d85152a4af686261db16385785c41f6ecc77
Kat-Ottman <43862300+Kat-Ottman@users.noreply.github.com>
2019-03-03 21:17:20 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*	Project:    P2 FrankenMap
 	Name:       Kat Ottman
-    Partner:    PARTNER'S NAME IF ANY
+    Partner:    Darryl McCottrell
     Class:      CSC 1820 Section 1
 */
 
@@ -20,13 +20,12 @@ class Node // creating a class that's variables will have two pieces of data
 	int frequency;
 	Node() { frequency = 1; }
 	vector<int> appears_on_lines;
-	//Would you add the count() here, inside the if(infile.is_open()) line, or right before the while loop?
-	//How do you have it so frequency adds on itself for each occurence of a word versus for every time that word appears frequency will equal 1?
 };
 
 int main(int argc, char *argv[])
 {
 	map<string, Node> words;
+	int countLine = 1;
 
 	if (argc < 2)
 	{
@@ -41,10 +40,22 @@ int main(int argc, char *argv[])
 
 		while (getline(infile, line)) //adding pairs based on text file lines to map
 		{
-			Node n = Node();
-			//n.appears_on_lines.push_back(n.countLine); //This line and the next comment work if there is only one occurence of each individual word
+			Node n;
+
+			auto it = words.find(line);
+			if ((*it) == words.at(line)) //if line exists in map
+			{
+				n = (*it).second;
+				n.frequency++;
+			}
+			else
+			{
+				n = Node();
+			}
+
+			n.appears_on_lines.push_back(countLine); //This line and the next comment work if there is only one occurence of each individual word
 			words.emplace(line, n);
-			//n.countLine++;
+			countLine++;
 		}
 
 		infile.close();
@@ -67,14 +78,14 @@ int main(int argc, char *argv[])
 		}
 		else //printing out word finding result
 		{
-			cout << word << " found " << words.at(word).frequency << " time(s) on lines: " << endl;
+			cout << word << " found " << (*it).second.frequency << " time(s) on lines: " << endl;
 			//frequency doesn't work unless there's only one occurence of the word
 
-			for (size_t positionInVector = 0; positionInVector < words.at(word).appears_on_lines.size(); positionInVector++) //print out lines where word appears in map
+			for (size_t positionInVector = 0; positionInVector < (*it).second.appears_on_lines.size(); positionInVector++) //print out lines where word appears in map
 			{
 
-				cout << words.at(word).appears_on_lines.at(positionInVector);
-				if (positionInVector != (words.at(word).appears_on_lines.size() - 1)) //getting rid of trailing comma
+				cout << (*it).second.appears_on_lines.at(positionInVector);
+				if (positionInVector != ((*it).second.appears_on_lines.size() - 1)) //getting rid of trailing comma
 				{
 					cout << ", ";
 				}
